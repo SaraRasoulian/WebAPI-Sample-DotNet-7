28c647319be76c4756f9188f4c9f66f96fd485f5
SaraRasoulian <sara.rasolian@gmail.com>
2023-08-28 16:10:05 +0330

    entity added

---
+++ src/Domain/Entities/Education.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities
+{
+    public class Education : EntityBase
+    {
+        public string? Degree { get; set; }
+
+        public string? FieldOfStudy { get; set; }
+
+        public string? School { get; set; }
+
+        public string? Description { get; set; }
+    }
+}
